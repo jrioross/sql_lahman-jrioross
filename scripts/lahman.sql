e870d0edaa30b2867bf67a5ce7fda7eb98ba338d
jrioross <j.rioross@gmail.com>
2022-02-10 21:41:05 -0600

    Sharpened a couple queries, added open-ended answers, and worked through first 2 bonus Qs.

--- scripts/lahman.sql
+++ scripts/lahman.sql
@@ -9,7 +9,7 @@ Which Vanderbilt player earned the most money in the majors?
 SELECT 
 	namefirst, 
 	namelast, 
-	SUM(salary) AS total_salary
+	SUM(salary)::numeric::money AS total_salary
 FROM people
 INNER JOIN salaries
 	USING (playerid)
@@ -34,22 +34,16 @@ and those with position "P" or "C" as "Battery".
 Determine the number of putouts made by each of these three groups in 2016.
 */
 
-WITH fielding_groups AS (
-	SELECT
-		pos,
-		CASE WHEN pos = 'OF' THEN 'Outfield' 
-			 WHEN pos IN ('SS', '1B', '2B', '3B') THEN 'Infield'
-			 WHEN pos IN ('P', 'C') THEN 'Battery'
-			 END AS pos_group,
-		po
-	FROM fielding
-	WHERE yearid = 2016
-)
 SELECT
-	pos_group,
+	CASE WHEN pos = 'OF' THEN 'Outfield' 
+	 	 WHEN pos IN ('SS', '1B', '2B', '3B') THEN 'Infield'
+	 	 WHEN pos IN ('P', 'C') THEN 'Battery'
+	 	 END AS pos_group,
 	SUM(po) AS total_po
-FROM fielding_groups
+FROM fielding
+WHERE yearid = 2016
 GROUP BY pos_group;
+
 
 /*
 Battery 41,424
@@ -69,21 +63,11 @@ If you want to see an example of this in action, check out this DataCamp video:
 https://campus.datacamp.com/courses/exploratory-data-analysis-in-sql/summarizing-and-aggregating-numeric-data?ex=6)
 */
 
-WITH so_hr_decades AS (
-	SELECT 
-		yearid,
-		teamid,
-		g,
-		FLOOR(yearid/10)*10 AS decade,
-		so,
-		hr
-	FROM teams
-)
 SELECT
-	decade,
-	ROUND(SUM(so)/(2.0*SUM(g)), 2) AS so_per_game,
-	ROUND(SUM(hr)/(2.0*SUM(g)), 2) AS hr_per_game
-FROM so_hr_decades
+	FLOOR(yearid/10)*10 AS decade,
+	ROUND(SUM(so)*2.0/(SUM(g)), 2) AS so_per_game,
+	ROUND(SUM(hr)*2.0/(SUM(g)), 2) AS hr_per_game
+FROM teams
 GROUP BY decade
 ORDER BY decade;
 
@@ -239,7 +223,9 @@ INNER JOIN people
 INNER JOIN managers
 	USING (playerid, yearid)
 WHERE awardid LIKE 'TSN%'
-ORDER BY namelast, yearid
+ORDER BY namelast, yearid;
+
+-- Davey Johnson and Jim Leyland
 
 ----------------------------------------------------------------------------------------7
 
@@ -264,7 +250,7 @@ GROUP BY playerid, namefirst, namelast
 HAVING sum(gs) >= 10
 ORDER BY salary_per_so DESC;
 
--- Matt Cain made a whopping $289,351.85
+-- Matt Cain made a whopping $289,351.85 per so
 
 ----------------------------------------------------------------------------------------8
 
@@ -390,3 +376,535 @@ WHERE yearid = 2016
 	AND season_hr > 0
 	AND season_hr = max_season_hr
 ORDER BY hr_2016 DESC;
+
+-----------------------/* OPEN-ENDED FROM WHEN I TOOK DA4 */----------------------------
+
+----------------------------------------------------------------------------------------11
+
+--Solution using ranks
+WITH ts AS(
+	SELECT yearid,
+			teamid,
+			SUM(salary) AS team_salary
+	FROM salaries
+	GROUP BY yearid, teamid
+	ORDER BY yearid, teamid
+),
+sal_w_rk AS(
+	SELECT t.yearid,
+		t.teamid,
+		ts.team_salary,
+		t.w,
+		RANK() OVER(PARTITION BY t.yearid ORDER BY ts.team_salary DESC) AS team_sal_rk,
+		RANK() OVER(PARTITION BY t.yearid ORDER BY t.w DESC) AS team_w_rk
+FROM teams AS t
+LEFT JOIN ts
+USING (yearid, teamid)
+WHERE t.yearid >= 2000
+ORDER BY t.yearid, t.w DESC
+	)
+SELECT team_sal_rk,
+		ROUND(AVG(team_w_rk), 1) AS avg_w_rk
+FROM sal_w_rk
+GROUP BY team_sal_rk
+ORDER BY team_sal_rk;
+--Salary is correlated with wins, though the slope isn't as steep as I'd expect (till #1)
+
+--Solution using correlation coefficient and regression slope
+WITH ts AS(
+	SELECT yearid,
+			teamid,
+			SUM(salary) AS team_salary
+	FROM salaries
+	GROUP BY yearid, teamid
+	ORDER BY yearid, teamid
+)
+SELECT corr(t.w, ts.team_salary) AS r_value,
+		regr_slope(t.w, ts.team_salary) * 10^7 AS w_per_ten_mil
+FROM teams AS t
+LEFT JOIN ts
+USING (yearid, teamid)
+WHERE t.yearid >= 2000;
+--Pretty high r-value considering the number of data values. About 1 win per 10 million dollars.
+
+----------------------------------------------------------------------------------------12
+/*
+12
+	a. Does there appear to be any correlation between attendance at home games and number of wins? 
+*/
+WITH w_att_rk AS (
+SELECT yearid,
+		teamid,
+		w,
+		attendance / ghome AS avg_h_att,
+		RANK() OVER(PARTITION BY yearid ORDER BY w) AS w_rk,
+		RANK() OVER(PARTITION BY yearid ORDER BY attendance / ghome) AS avg_h_att_rk
+FROM teams
+WHERE attendance / ghome IS NOT NULL
+AND yearid >= 1961 						--MLB institutes 162 game season
+ORDER BY yearid, teamid
+)
+SELECT avg_h_att_rk,
+		ROUND(AVG(w_rk), 1) AS avg_w_rk,
+		CORR(avg_h_att_rk, AVG(w_rk)) OVER() as correlation
+FROM w_att_rk
+GROUP BY avg_h_att_rk
+ORDER BY avg_h_att_rk;
+--Very strong correlation between wins and home game attendance.
+
+
+/*
+12
+
+	b. Do teams that win the world series see a boost in attendance the following year?
+	   What about teams that made the playoffs?
+	   Making the playoffs means either being a division winner or a wild card winner.
+*/
+--After World Series Win
+WITH att_comp AS (
+SELECT yearid,
+		name,
+		attendance / ghome AS att_g,
+		lead(attendance / ghome) OVER(PARTITION BY name ORDER BY yearid) AS att_g_next_year,
+		lead(attendance / ghome) OVER(PARTITION BY name ORDER BY yearid) - (attendance/ghome) AS difference
+FROM teams AS t
+)
+SELECT ROUND(AVG(difference), 1) AS avg_att_dif
+FROM att_comp
+INNER JOIN teams AS t
+USING (yearid, name)
+WHERE wswin = 'Y';
+--Attendance improves, on average, by 267.1 people per home game.
+
+--After Playoff Berth
+WITH att_comp AS (
+SELECT yearid,
+		name,
+		attendance / ghome AS att_g,
+		lead(attendance / ghome) OVER(PARTITION BY name ORDER BY yearid) AS att_g_next_year,
+		lead(attendance / ghome) OVER(PARTITION BY name ORDER BY yearid) - (attendance/ghome) AS difference
+FROM teams AS t
+)
+SELECT ROUND(AVG(difference), 1) AS avg_att_dif
+FROM att_comp
+INNER JOIN teams AS t
+USING (yearid, name)
+WHERE wcwin = 'Y' OR divwin = 'Y';
+--Attendance improves, on average, by 561.9 people per home game.
+
+----------------------------------------------------------------------------------------13
+/*
+13. It is thought that since left-handed pitchers are more rare,
+causing batters to face them less often, that they are more effective.
+Investigate this claim and present evidence to either support or dispute this claim.
+First, determine just how rare left-handed pitchers are compared with right-handed pitchers.
+Are left-handed pitchers more likely to win the Cy Young Award?
+Are they more likely to make it into the hall of fame?
+*/
+--Relative frequency of L vs R pitchers
+SELECT SUM(CASE WHEN throws = 'L' THEN 1 ELSE 0 END) AS ct_L,
+		ROUND(AVG(CASE WHEN throws = 'L' THEN 1 ELSE 0 END), 4) AS perc_L,
+		SUM(CASE WHEN throws = 'R' THEN 1 ELSE 0 END) AS ct_R,
+		ROUND(AVG(CASE WHEN throws = 'R' THEN 1 ELSE 0 END), 4) AS perc_R
+FROM people
+WHERE playerid IN
+	(SELECT DISTINCT playerid
+	FROM pitching
+	)
+--L: 26.63%, R: 71.01%
+
+--Relative frequency of Cy Young Awards (relative to all and relative to group size)
+
+WITH cy_young AS (
+	SELECT *
+	FROM awardsplayers
+	WHERE awardid = 'Cy Young Award'
+	),
+left_pitchers AS (
+	SELECT *
+	FROM people
+	WHERE playerid IN
+		(SELECT DISTINCT playerid
+		FROM pitching
+		)
+	AND throws = 'L'
+	),
+right_pitchers AS (
+	SELECT *
+	FROM people
+	WHERE playerid IN
+		(SELECT DISTINCT playerid
+		FROM pitching
+		)
+	AND throws = 'R'
+	)
+SELECT ROUND(AVG(CASE WHEN p.throws = 'L' THEN 1
+		  		WHEN p.throws = 'R' THEN 0 END), 4) AS perc_CY_L,
+		ROUND(AVG(CASE WHEN p.throws = 'R' THEN 1
+		  		WHEN p.throws = 'L' THEN 0 END), 4) AS perc_CY_R
+FROM people AS p
+INNER JOIN cy_young
+USING (playerid)
+--L: 33.04%, R: 66.96%
+
+WITH cy_young AS (
+	SELECT *
+	FROM awardsplayers
+	WHERE awardid = 'Cy Young Award'
+	),
+left_pitchers AS (
+	SELECT *
+	FROM people
+	WHERE playerid IN
+		(SELECT DISTINCT playerid
+		FROM pitching
+		)
+	AND throws = 'L'
+	),
+right_pitchers AS (
+	SELECT *
+	FROM people
+	WHERE playerid IN
+		(SELECT DISTINCT playerid
+		FROM pitching
+		)
+	AND throws = 'R'
+	)
+SELECT 'Left' AS Arm,
+		ROUND(AVG(CASE WHEN awardid = 'Cy Young Award' THEN 1
+		  				ELSE 0 END), 4) AS perc_CY
+FROM left_pitchers AS l
+LEFT JOIN cy_young
+USING (playerid)
+UNION
+SELECT 'Right' AS Arm,
+		ROUND(AVG(CASE WHEN awardid = 'Cy Young Award' THEN 1
+		  				ELSE 0 END), 4) AS perc_CY
+FROM right_pitchers AS r
+LEFT JOIN cy_young
+USING (playerid)
+--1.49% of left handers win the Cy Young, while 1.13% of right handers win the Cy Young
+
+--Relative frequency of HOF Induction
+WITH hof_pitchers AS (
+	SELECT *
+	FROM halloffame
+	INNER JOIN pitching
+	USING (playerid)
+	WHERE inducted = 'Y'
+	)
+SELECT ROUND(AVG(CASE WHEN throws = 'L' THEN 1
+		  		ELSE 0 END), 4) AS perc_HOF_L_pitch,
+		ROUND(AVG(CASE WHEN throws = 'R' THEN 1
+		  		ELSE 0 END), 4) AS perc_HOF_R_pitch
+FROM hof_pitchers
+INNER JOIN people
+USING (playerid)
+--Percent of HOF pitchers who are lefty: 22.51%, righty: 77.49%
+				
+WITH hof_pitchers AS (
+	SELECT *
+	FROM halloffame
+	INNER JOIN pitching
+	USING (playerid)
+	WHERE inducted = 'Y'
+	),
+left_pitchers AS (
+	SELECT *
+	FROM people
+	WHERE playerid IN
+		(SELECT DISTINCT playerid
+		FROM pitching
+		)
+	AND throws = 'L'
+	),
+right_pitchers AS (
+	SELECT *
+	FROM people
+	WHERE playerid IN
+		(SELECT DISTINCT playerid
+		FROM pitching
+		)
+	AND throws = 'R'
+	)
+SELECT 'Left' AS Arm,
+		ROUND(AVG(CASE WHEN inducted = 'Y' THEN 1
+		  				ELSE 0 END), 4) AS perc_HOF
+FROM left_pitchers AS l
+LEFT JOIN hof_pitchers
+USING (playerid)
+UNION
+SELECT 'Right' AS Arm,
+		ROUND(AVG(CASE WHEN inducted = 'Y' THEN 1
+		  				ELSE 0 END), 4) AS perc_HOF
+FROM right_pitchers AS r
+LEFT JOIN hof_pitchers
+USING (playerid)
+--Percent of lefty pitchers who enter HOF: 11.18%, Righties: 14.02%
+
+---------------------------------------------------------------------------------------- Bonus 1
+
+/*
+1. In this question, you'll get to practice correlated subqueries and learn about 
+the LATERAL keyword. Note: This could be done using window functions, 
+but we'll do it in a different way in order to revisit correlated subqueries 
+and see another keyword - LATERAL.
+
+a. First, write a query utilizing a correlated subquery to find the team with the most wins from each league in 2016.
+
+If you need a hint, you can structure your query as follows:
+
+SELECT DISTINCT lgid, ( <Write a correlated subquery here that will pull the teamid for the team with the highest number of wins from each league> )
+FROM teams t
+WHERE yearid = 2016;
+*/
+
+SELECT DISTINCT 
+	lgid,
+	(SELECT name
+	 FROM teams AS st
+	 WHERE t.lgid = st.lgid
+	 	AND yearid = 2016
+	 ORDER BY w DESC
+	 LIMIT 1)
+FROM teams AS t
+WHERE yearid = 2016;
+
+/*
+b. One downside to using correlated subqueries is that you can only return exactly
+one row and one column. This means, for example that if we wanted to pull in
+not just the teamid but also the number of wins, we couldn't do so using just 
+a single subquery. (Try it and see the error you get). 
+Add another correlated subquery to your query on the previous part so that 
+your result shows not just the teamid but also the number of wins by that team.
+*/
+
+SELECT DISTINCT 
+	lgid,
+	(SELECT name
+	 FROM teams AS st
+	 WHERE t.lgid = st.lgid
+	 	AND yearid = 2016
+	 ORDER BY w DESC
+	 LIMIT 1),
+	(SELECT w
+	 FROM teams AS st
+	 WHERE t.lgid = st.lgid
+	 	AND yearid = 2016
+	 ORDER BY w DESC
+	 LIMIT 1)	 
+FROM teams AS t
+WHERE yearid = 2016;
+
+/*
+c. If you are interested in pulling in the top (or bottom) values by group,
+you can also use the DISTINCT ON expression (https://www.postgresql.org/docs/9.5/sql-select.html#SQL-DISTINCT). 
+Rewrite your previous query into one which uses DISTINCT ON to return 
+the top team by league in terms of number of wins in 2016. 
+Your query should return the league, the teamid, and the number of wins.
+*/
+
+SELECT DISTINCT ON (lgid)
+	lgid,
+	name,
+	w
+FROM teams AS t
+WHERE yearid = 2016
+ORDER BY lgid, w DESC;
+
+/*
+d. If we want to pull in more than one column in our correlated subquery, 
+another way to do it is to make use of the LATERAL keyword 
+(https://www.postgresql.org/docs/9.4/queries-table-expressions.html#QUERIES-LATERAL). 
+This allows you to write subqueries in FROM that make reference to 
+columns from previous FROM items. This gives us the flexibility to 
+pull in or calculate multiple columns or multiple rows (or both). 
+Rewrite your previous query using the LATERAL keyword so that 
+your result shows the teamid and number of wins for the team with 
+the most wins from each league in 2016. 
+
+If you want a hint, you can structure your query as follows:
+
+SELECT *
+FROM (SELECT DISTINCT lgid 
+	  FROM teams
+	  WHERE yearid = 2016) AS leagues,
+	  LATERAL ( <Fill in a subquery here to retrieve the teamid and number of wins> ) as top_teams;
+*/
+
+SELECT *
+FROM (SELECT DISTINCT lgid 
+	  FROM teams
+	  WHERE yearid = 2016) AS leagues,
+	  LATERAL (SELECT
+			  		name,
+			  		w
+			  	FROM teams AS t
+			  	WHERE leagues.lgid = t.lgid
+			   		AND yearid = 2016
+			  	ORDER BY w DESC
+			  	LIMIT 1) as top_teams;
+
+/*
+e. Finally, another advantage of the LATERAL keyword over using correlated 
+subqueries is that you return multiple result rows. 
+(Try to return more than one row in your correlated subquery from above 
+and see what type of error you get). Rewrite your query on the previous 
+problem sot that it returns the top 3 teams from each league 
+in term of number of wins. Show the teamid and number of wins.
+*/
+
+SELECT *
+FROM (SELECT DISTINCT lgid 
+	  FROM teams
+	  WHERE yearid = 2016) AS leagues,
+	  LATERAL (SELECT
+			  		name,
+			  		w
+			  	FROM teams AS t
+			  	WHERE leagues.lgid = t.lgid
+			   		AND yearid = 2016
+			  	ORDER BY w DESC
+			  	LIMIT 3) as top_teams;
+
+---------------------------------------------------------------------------------------- Bonus 2
+
+/*
+2. Another advantage of lateral joins is for when you create calculated columns. 
+In a regular query, when you create a calculated column, you cannot refer to it when you create other 
+calculated columns. This is particularly useful if you want to reuse a calculated column multiple times. 
+For example,
+
+SELECT 
+	teamid,
+	w,
+	l,
+	w + l AS total_games,
+	w*100.0 / total_games AS winning_pct
+FROM teams
+WHERE yearid = 2016
+ORDER BY winning_pct DESC;
+
+results in the error that "total_games" does not exist. However, I can restructure this query using the LATERAL keyword.
+
+SELECT
+	teamid,
+	w,
+	l,
+	total_games,
+	w*100.0 / total_games AS winning_pct
+FROM teams t,
+LATERAL (
+	SELECT w + l AS total_games
+) AS tg
+WHERE yearid = 2016
+ORDER BY winning_pct DESC;
+
+a. Write a query which, for each player in the player table, assembles their 
+birthyear, birthmonth, and birthday into a single column called birthdate which is of the date type.
+*/
+
+SELECT (birthyear || '-' || birthmonth || '-' || birthday)::date AS birthdate
+FROM people;
+
+/*
+b. Use your previous result inside a subquery using LATERAL to calculate for each player their age at debut and age at retirement.
+(Hint: It might be useful to check out the PostgreSQL date and time functions 
+https://www.postgresql.org/docs/8.4/functions-datetime.html).
+*/
+
+SELECT 
+	namefirst,
+	namelast,
+	AGE(debut::date, birthdate) AS age_debut, 
+	AGE(finalgame::date, birthdate) AS age_retire
+FROM people,
+LATERAL (
+	SELECT (birthyear || '-' || birthmonth || '-' || birthday)::date AS birthdate
+	) AS bd
+
+/*
+c. Who is the youngest player to ever play in the major leagues?
+*/
+
+SELECT 
+	namefirst,
+	namelast,
+	AGE(debut::date, birthdate) AS age_debut, 
+	AGE(finalgame::date, birthdate) AS age_retire
+FROM people,
+LATERAL (
+	SELECT (birthyear || '-' || birthmonth || '-' || birthday)::date AS birthdate
+	) AS bd
+ORDER BY age_debut
+LIMIT 1;
+
+-- Joe Nuxhall debuted at age 15 years, 10 months, and 11 days
+
+/*
+d. Who is the oldest player to player in the major leagues? You'll likely have a lot of null values 
+resulting in your age at retirement calculation. Check out the documentation on sorting rows here 
+https://www.postgresql.org/docs/8.3/queries-order.html about how you can change how null values are sorted.
+*/
+
+SELECT 
+	namefirst,
+	namelast,
+	AGE(debut::date, birthdate) AS age_debut, 
+	AGE(finalgame::date, birthdate) AS age_retire
+FROM people,
+LATERAL (
+	SELECT (birthyear || '-' || birthmonth || '-' || birthday)::date AS birthdate
+	) AS bd
+ORDER BY age_retire DESC NULLS LAST
+LIMIT 1;
+
+-- Satchel Paige's last game was at age 59 years, 2 months, 18 days
+
+---------------------------------------------------------------------------------------- Bonus 3
+
+/*
+3. For this question, you will want to make use of RECURSIVE CTEs 
+(see https://www.postgresql.org/docs/13/queries-with.html). 
+The RECURSIVE keyword allows a CTE to refer to its own output. Recursive CTEs are useful for navigating 
+network datasets such as social networks, logistics networks, or employee hierarchies 
+(who manages who and who manages that person). To see an example of the last item, see this tutorial: 
+https://www.postgresqltutorial.com/postgresql-recursive-query/. 
+In the next couple of weeks, you'll see how the graph database Neo4j can easily work with such datasets, 
+but for now we'll see how the RECURSIVE keyword can pull it off (in a much less efficient manner) in PostgreSQL. 
+(Hint: You might find it useful to look at this blog post when attempting to answer the following questions: 
+https://data36.com/kevin-bacon-game-recursive-sql/.)
+
+a. Willie Mays holds the record of the most All Star Game starts with 18. 
+How many players started in an All Star Game with Willie Mays? 
+(A player started an All Star Game if they appear in the allstarfull 
+table with a non-null startingpos value).
+*/
+
+SELECT *
+FROM allstarfull AS as1
+LEFT JOIN allstarfull AS as2
+	USING (gameid)
+WHERE as1.playerid <> as2.playerid;
+
+/*
+b. How many players didn't start in an All Star Game with Willie Mays but started an All Star Game 
+with another player who started an All Star Game with Willie Mays? 
+For example, Graig Nettles never started an All Star Game with Willie Mayes, but he did start the 
+1975 All Star Game with Blue Vida who started the 1971 All Star Game with Willie Mays.
+*/
+
+
+
+/*
+c. We'll call two players connected if they both started in the same All Star Game. Using this, we can 
+find chains of players. For example, one chain from Carlton Fisk to Willie Mays is as follows: 
+Carlton Fisk started in the 1973 All Star Game with Rod Carew who started in the 1972 All Star Game 
+with Willie Mays. Find a chain of All Star starters connecting Babe Ruth to Willie Mays. 
+*/
+
+
+
+/*
+d. How large a chain do you need to connect Derek Jeter to Willie Mays?
+*/
